85471756a72b836531ea4dd3b06f655e675c2305
raheel khan <irahilkhan@gmail.com>
2019-02-08 00:44:11 +0500

    initial upload of vue js

---
+++ src/components/Heading.vue
@@ -0,0 +1,14 @@
+<template>
+<div>
+	<h1> My Name is {{name}}</h1>
+</div>
+</template>
+<script>
+export default{
+		name: 'heading',
+		props: ['name'],
+		data:{
+			name:'raheel',
+		}
+	}
+</script>
